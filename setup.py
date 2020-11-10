9db0f6c996a3ae830f7a8f2bce991052806ac97d
Lizw_qq2500xxxxxx <2500783577@qq.com>
2020-11-10 15:27:18 +0800

    test

--- setup.py
+++ setup.py
@@ -5,19 +5,23 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mycv",
+    name="my_test_pip_cv_653426",  # 这个名字要独一无二的
     version="1.0",
     author="lzw",
-    author_email="lv.llf@qq.com",
+    author_email="your email@qq.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Faner227/PIPTEST",
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=['opencv-python>=4.2.0.34'],
+    py_requires=["example_pkg"],  # 这是你存放python代码的目录
     python_requires='>=3.6',
 )
+
+
+#     # packages=setuptools.find_packages(),
